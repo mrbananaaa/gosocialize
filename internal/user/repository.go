db92140480d21d90becfda6c5966dacd2027f653
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-03 21:41:51 +0700

    feat: create postgres user repo implementation

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -13,3 +13,22 @@ type Repository interface {
 }
 
 // TODO: implementations goes here, e.g., PostgresRepository
+
+type PostgresRepository struct {
+}
+
+func NewRepository() *PostgresRepository {
+	return &PostgresRepository{}
+}
+
+func (r *PostgresRepository) Create(ctx context.Context, user *User) error {
+	return nil
+}
+
+func (r *PostgresRepository) GetByID(ctx context.Context, id uuid.UUID) (*User, error) {
+	return nil, nil
+}
+
+func (r *PostgresRepository) GetByEmail(ctx context.Context, email string) (*User, error) {
+	return nil, nil
+}
