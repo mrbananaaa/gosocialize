b0817d168af68e3697e3feef5d1dd4255aa99c18
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-03 17:54:13 +0700

    feat: bootstrap server package

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,7 +1,59 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"time"
+)
 
 type Server struct {
-	httpServer http.Server
+	httpServer *http.Server
+}
+
+func NewServer() (*Server, error) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		d, err := json.Marshal(struct {
+			Message string `json:"message"`
+		}{
+			Message: "OK",
+		})
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(d)
+	})
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	return &Server{
+		httpServer: httpServer,
+	}, nil
+}
+
+func (s *Server) Run() error {
+	// TODO: Use environment variable from Config
+	log.Println("Server listening on :8080")
+	if err := s.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
 }
