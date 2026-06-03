d2104566d1e5c61d41fc2d593291d8ecd8562ce6
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-03 17:55:14 +0700

    feat: implement server package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/mrbananaaa/gosocialize/internal/api"
+)
 
 func main() {
-	fmt.Println("Hello world! I'm back!")
+	s, err := api.NewServer()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := s.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Shutting down server...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(shutdownCtx); err != nil {
+		log.Println("Failed to shutdown http server")
+		log.Fatal(err)
+	}
+
+	log.Println("Server closed gracefully")
 }
