907ce813b60efd10fc5b69d559ac3eddf547b8c2
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-09 19:45:54 +0700

    feat: add seeding post function

---
+++ internal/platform/database/postgres/seeds/posts.go
@@ -0,0 +1,49 @@
+package seeds
+
+import (
+	"context"
+	"errors"
+	"math/rand/v2"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/google/uuid"
+	"github.com/mrbananaaa/gosocialize/internal/platform/database/postgres/sqlc"
+)
+
+func SeedPosts(
+	ctx context.Context,
+	q *sqlc.Queries,
+	count int,
+	users []sqlc.User,
+) ([]sqlc.Post, error) {
+	var posts []sqlc.Post
+
+	if len(users) == 0 {
+		return nil, errors.New("Users ids are empty")
+	}
+
+	for range count {
+		now := time.Now()
+		randomUser := rand.IntN(len(users))
+		userID := users[randomUser].ID
+
+		p := sqlc.CreatePostParams{
+			ID:        uuid.New(),
+			UserID:    userID,
+			Title:     gofakeit.LoremIpsumSentence(16),
+			Content:   gofakeit.Paragraph(),
+			CreatedAt: now,
+			UpdatedAt: now,
+		}
+
+		err := q.CreatePost(ctx, p)
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, sqlc.Post(p))
+	}
+
+	return posts, nil
+}
