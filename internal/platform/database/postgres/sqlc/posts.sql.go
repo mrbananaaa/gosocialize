b322576c9caa0fd7eda9770c98f19e61a93ec460
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-09 20:04:38 +0700

    feat: create getPost query

--- internal/platform/database/postgres/sqlc/posts.sql.go
+++ internal/platform/database/postgres/sqlc/posts.sql.go
@@ -40,3 +40,37 @@ func (q *Queries) CreatePost(ctx context.Context, arg CreatePostParams) error {
 	)
 	return err
 }
+
+const getPost = `-- name: GetPost :many
+SELECT
+  id, user_id, title, content, created_at, updated_at
+FROM
+  posts
+`
+
+func (q *Queries) GetPost(ctx context.Context) ([]Post, error) {
+	rows, err := q.db.Query(ctx, getPost)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Post
+	for rows.Next() {
+		var i Post
+		if err := rows.Scan(
+			&i.ID,
+			&i.UserID,
+			&i.Title,
+			&i.Content,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
--- internal/platform/database/postgres/sqlc/queries/posts.sql
+++ internal/platform/database/postgres/sqlc/queries/posts.sql
@@ -4,3 +4,9 @@ INSERT INTO posts (
 ) VALUES (
   $1, $2, $3, $4, $5, $6
 );
+
+-- name: GetPost :many
+SELECT
+  id, user_id, title, content, created_at, updated_at
+FROM
+  posts;
