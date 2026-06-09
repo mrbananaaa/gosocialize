d76369efdddc8208e548de606296608a1d7c8794
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-09 17:21:11 +0700

    feat: add post entity

--- internal/post/entity.go
+++ internal/post/entity.go
@@ -1,1 +1,17 @@
 package post
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Post struct {
+	ID        uuid.UUID
+	UserID    uuid.UUID
+	Title     string
+	Content   string
+	Tags      []string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
