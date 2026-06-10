815c42e1ce740245342e2dd9f30883498d1baa62
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-10 09:03:39 +0700

    feat: add ListPosts query

--- internal/platform/database/postgres/sqlc/queries/posts.sql
+++ internal/platform/database/postgres/sqlc/queries/posts.sql
@@ -10,3 +10,21 @@ SELECT
   id, user_id, title, content, created_at, updated_at
 FROM
   posts;
+
+-- name: ListPosts :many
+SELECT
+  id, user_id, title, content, created_at, updated_at
+FROM
+  posts
+WHERE
+  (
+    sqlc.arg(cursor_created_at)::timestamptz IS NULL
+    OR (created_at, id) < (
+      sqlc.arg(cursor_created_at)::timestamptz,
+      sqlc.arg(cursor_id)::uuid
+    )
+  )
+ORDER BY
+  created_at DESC, id DESC
+LIMIT
+  sqlc.arg(pagination_limit);
