53da21ebd37a019fcea443c6baf0f942b8dcace2
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-10 18:44:03 +0700

    refactor: removing unused queries

--- internal/platform/database/postgres/sqlc/queries/posts.sql
+++ internal/platform/database/postgres/sqlc/queries/posts.sql
@@ -5,30 +5,6 @@ INSERT INTO posts (
   $1, $2, $3, $4, $5, $6
 );
 
--- name: GetPost :many
-SELECT
-  id, user_id, title, content, created_at, updated_at
-FROM
-  posts;
-
--- name: ListPosts :many
-SELECT
-  id, user_id, title, content, created_at, updated_at
-FROM
-  posts
-WHERE
-  (
-    sqlc.arg(cursor_created_at)::timestamptz IS NULL
-    OR (created_at, id) < (
-      sqlc.arg(cursor_created_at)::timestamptz,
-      sqlc.arg(cursor_id)::uuid
-    )
-  )
-ORDER BY
-  created_at DESC, id DESC
-LIMIT
-  sqlc.arg(pagination_limit);
-
 -- name: ListPostsFirst :many
 SELECT
   id, user_id, title, content, created_at, updated_at
