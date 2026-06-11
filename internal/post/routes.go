892f7173c7ea6c6c1a2e4e87b326b13d24447055
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-12 00:10:39 +0700

    feat: add auth middleware param

--- internal/post/routes.go
+++ internal/post/routes.go
@@ -1,8 +1,13 @@
 package post
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/mrbananaaa/gosocialize/internal/middlewares"
+)
 
-func (h *Handler) Routes() chi.Router {
+func (h *Handler) Routes(
+	authMiddleware *middlewares.AuthMiddleware,
+) chi.Router {
 	r := chi.NewRouter()
 
 	r.Get("/", h.ListPosts)
@@ -10,6 +15,12 @@ func (h *Handler) Routes() chi.Router {
 
 	// TODO: refactor and move create post with auth
 	r.Post("/", h.CreatePost)
+
+	r.Group(func(u chi.Router) {
+		u.Use(authMiddleware.WithAccessToken)
+
+		u.Patch("/{postID}", h.UpdatePost)
+	})
 
 	return r
 }
