22d79a16f38e39d5ce71c9ac78f6ff0f3709c1cf
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-20 01:16:06 +0700

    feat: add CountFollowers and CountFollowing query

--- backend/internal/platform/database/postgres/queries/follows.sql
+++ backend/internal/platform/database/postgres/queries/follows.sql
@@ -1,18 +1,28 @@
 -- name: FollowUser :exec
-INSERT INTO follows (
-  followee_id, follower_id
-) VALUES (
-  $1, $2
-);
+INSERT INTO follows (follower_id, followee_id)
+VALUES ($1, $2)
+ON CONFLICT DO NOTHING;
 
 -- name: UnfollowUser :exec
 DELETE
 FROM follows
-WHERE
-  followee_id = $1 AND follower_id = $2;
+WHERE follower_id = $1 
+  AND followee_id = $2;
 
--- name: FollowersCount :one
-SELECT
-  COUNT(*)
+-- name: IsFollowing :one
+SELECT EXISTS (
+  SELECT 1
+  FROM follows
+  WHERE follower_id = $1
+    AND followee_id = $2
+);
+
+-- name: CountFollowers :one
+SELECT COUNT(*)
 FROM follows
 WHERE followee_id = $1;
+
+-- name: CountFollowing :one
+SELECT COUNT(*)
+FROM follows
+WHERE follower_id = $1;
