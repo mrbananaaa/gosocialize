26b507dea7ba54ff64be12f2fb280722534d5ebf
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-20 01:17:17 +0700

    feat: follow service implementation

--- backend/internal/follow/service.go
+++ backend/internal/follow/service.go
@@ -2,15 +2,18 @@ package follow
 
 import (
 	"context"
+	"errors"
 
 	"github.com/google/uuid"
+	"github.com/mrbananaaa/gosocialize/internal/platform/apperr"
+	"github.com/mrbananaaa/gosocialize/internal/platform/database/postgres"
 	"github.com/mrbananaaa/gosocialize/internal/platform/db"
 	"github.com/mrbananaaa/gosocialize/store"
 )
 
 type Service interface {
-	Follow(ctx context.Context, followeeID, followerID uuid.UUID) error
-	Unfollow(ctx context.Context, followeeID, followerID uuid.UUID) error
+	Follow(ctx context.Context, followerID, followeeID uuid.UUID) error
+	Unfollow(ctx context.Context, followerID, followeeID uuid.UUID) error
 	FollowerCount(ctx context.Context, userID uuid.UUID) (int, error)
 }
 
@@ -24,21 +27,70 @@ func NewService(store *store.Store) Service {
 	}
 }
 
-func (s *followService) Follow(ctx context.Context, followeeID, followerID uuid.UUID) error {
-	return s.store.Q.FollowUser(ctx, db.FollowUserParams{
-		FolloweeID: followeeID,
+func (s *followService) Follow(ctx context.Context, followerID, followeeID uuid.UUID) error {
+	_, err := s.store.Q.FindUserByID(ctx, followeeID)
+	if err != nil {
+		err = postgres.PgErrMapper(err)
+
+		if errors.Is(err, apperr.ErrNotFound) {
+			return apperr.New(apperr.Code.NotFound, "can't follow non-existent user")
+		}
+
+		return err
+	}
+
+	err = s.store.Q.FollowUser(ctx, db.FollowUserParams{
 		FollowerID: followerID,
+		FolloweeID: followeeID,
 	})
+
+	return postgres.PgErrMapper(err)
 }
 
-func (s *followService) Unfollow(ctx context.Context, followeeID, followerID uuid.UUID) error {
-	return s.store.Q.UnfollowUser(ctx, db.UnfollowUserParams{
-		FolloweeID: followeeID,
+func (s *followService) Unfollow(ctx context.Context, followerID, followeeID uuid.UUID) error {
+	_, err := s.store.Q.FindUserByID(ctx, followeeID)
+	if err != nil {
+		err = postgres.PgErrMapper(err)
+
+		if errors.Is(err, apperr.ErrNotFound) {
+			return apperr.New(apperr.Code.NotFound, "can't found followed user")
+		}
+
+		return err
+	}
+
+	isFollowing, err := s.store.Q.IsFollowing(ctx, db.IsFollowingParams{
 		FollowerID: followerID,
+		FolloweeID: followeeID,
 	})
+	if err != nil {
+		return postgres.PgErrMapper(err)
+	}
+
+	if !isFollowing {
+		return apperr.New(apperr.Code.BadRequest, "you're not following this user")
+	}
+
+	err = s.store.Q.UnfollowUser(ctx, db.UnfollowUserParams{
+		FollowerID: followerID,
+		FolloweeID: followeeID,
+	})
+
+	return postgres.PgErrMapper(err)
 }
 
 func (s *followService) FollowerCount(ctx context.Context, userID uuid.UUID) (int, error) {
-	count, err := s.store.Q.FollowersCount(ctx, userID)
-	return int(count), err
+	_, err := s.store.Q.FindUserByID(ctx, userID)
+	if err != nil {
+		err = postgres.PgErrMapper(err)
+
+		if errors.Is(err, apperr.ErrNotFound) {
+			return 0, apperr.New(apperr.Code.NotFound, "can't count follower of non-existent user")
+		}
+
+		return 0, err
+	}
+
+	count, err := s.store.Q.CountFollowers(ctx, userID)
+	return int(count), postgres.PgErrMapper(err)
 }
--- backend/internal/follow/service_test.go
+++ backend/internal/follow/service_test.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/mrbananaaa/gosocialize/internal/follow"
+	"github.com/mrbananaaa/gosocialize/internal/platform/apperr"
 	"github.com/mrbananaaa/gosocialize/internal/testutil"
 	"github.com/mrbananaaa/gosocialize/store"
 	"github.com/stretchr/testify/require"
 )
 
 func TestFollow(t *testing.T) {
-	t.Run("success - user can follow other user", func(t *testing.T) {
-		env, svc := newFollowService(t)
+	env, svc := newFollowService(t)
 
+	t.Run("success - user can follow other user", func(t *testing.T) {
 		user1 := env.CreateUser(t)
 		user2 := env.CreateUser(t)
 
@@ -21,19 +22,81 @@ func TestFollow(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	t.Run("fail - cannot follow user that doesn't exits", func(t *testing.T) {
-		env, svc := newFollowService(t)
-
+	t.Run("fail - can't follow non-existent user", func(t *testing.T) {
 		follower := env.CreateUser(t)
 
-		err := svc.Follow(t.Context(), uuid.New(), follower.ID)
-		require.Error(t, err)
+		err := svc.Follow(t.Context(), follower.ID, uuid.New())
+
+		var appErr *apperr.Error
+		require.ErrorAs(t, err, &appErr)
+		require.Equal(t, "can't follow non-existent user", appErr.Message)
 	})
 }
 
 func TestUnfollow(t *testing.T) {
-	t.Run("success - user can unfollow", func(t *testing.T) {
+	env, svc := newFollowService(t)
 
+	t.Run("success - user can unfollow", func(t *testing.T) {
+		user1 := env.CreateUser(t)
+		user2 := env.CreateUser(t)
+
+		err := svc.Follow(t.Context(), user1.ID, user2.ID)
+		require.NoError(t, err)
+
+		err = svc.Unfollow(t.Context(), user1.ID, user2.ID)
+		require.NoError(t, err)
+
+		count, err := env.Q.CountFollowers(t.Context(), user2.ID)
+		require.NoError(t, err)
+		require.Equal(t, 0, int(count))
+	})
+
+	t.Run("fail - can't unfollow unfollowed users", func(t *testing.T) {
+		user := env.CreateUser(t)
+		follower := env.CreateUser(t)
+
+		err := svc.Unfollow(t.Context(), follower.ID, user.ID)
+
+		var appErr *apperr.Error
+		require.ErrorAs(t, err, &appErr)
+		require.Equal(t, "you're not following this user", appErr.Message)
+	})
+
+	t.Run("fail - user can't unfollow non-existent user", func(t *testing.T) {
+		follower := env.CreateUser(t)
+
+		err := svc.Unfollow(t.Context(), follower.ID, uuid.New())
+
+		var appErr *apperr.Error
+		require.ErrorAs(t, err, &appErr)
+		require.Equal(t, "can't found followed user", appErr.Message)
+	})
+}
+
+func TestFollowerCount(t *testing.T) {
+	env, svc := newFollowService(t)
+
+	t.Run("success - followers counted", func(t *testing.T) {
+		user := env.CreateUser(t)
+		followerTotal := 15
+
+		for range followerTotal {
+			follower := env.CreateUser(t)
+			err := svc.Follow(t.Context(), follower.ID, user.ID)
+			require.NoError(t, err)
+		}
+
+		count, err := svc.FollowerCount(t.Context(), user.ID)
+		require.NoError(t, err)
+		require.Equal(t, followerTotal, count)
+	})
+
+	t.Run("fail - can't count follower of non-existent user", func(t *testing.T) {
+		_, err := svc.FollowerCount(t.Context(), uuid.New())
+
+		var appErr *apperr.Error
+		require.ErrorAs(t, err, &appErr)
+		require.Equal(t, "can't count follower of non-existent user", appErr.Message)
 	})
 }
 
