bd0d5d225277db93b492cec7329b006ea868c3ad
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-06-22 07:15:23 +0700

    feat: add GetUserFeeds method

--- backend/internal/feed/service.go
+++ backend/internal/feed/service.go
@@ -4,8 +4,12 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/mrbananaaa/gosocialize/internal/platform/apperr"
 	"github.com/mrbananaaa/gosocialize/internal/platform/database/postgres"
+	"github.com/mrbananaaa/gosocialize/internal/platform/db"
 	"github.com/mrbananaaa/gosocialize/internal/post"
+	"github.com/mrbananaaa/gosocialize/pkg/pagination"
 	"github.com/mrbananaaa/gosocialize/store"
 )
 
@@ -19,24 +23,57 @@ type feedService struct {
 
 func NewService(
 	s *store.Store,
-) *Service {
-	return &Service{
+) Service {
+	return &feedService{
 		store: s,
 	}
 }
 
-func (s *Service) GetFeeds(
+type GetUserFeedsPayload struct {
+	Posts      []post.Post
+	NextCursor string
+	HasMore    bool
+}
+
+func (s *feedService) GetUserFeeds(
 	ctx context.Context,
 	userID uuid.UUID,
-) ([]post.Post, error) {
-	feeds, err := s.store.Q.ListPostForFeeds(ctx, userID)
+	cursorQuery pagination.CursorQueryParam,
+) (*GetUserFeedsPayload, error) {
+	cursor, err := pagination.DecodeCursor(cursorQuery.Cursor)
 	if err != nil {
-		return nil, postgres.PgErrMapper(err)
+		return nil, apperr.New(apperr.Code.BadRequest, "invalid cursor")
 	}
 
-	f := make([]post.Post, 0, len(feeds))
-	for _, p := range feeds {
-		f = append(f, post.Post{
+	paginationLimit := cursorQuery.Limit + 1
+
+	var rows []db.Post
+	if cursor == nil {
+		rows, err = s.store.Q.UserFeedsFirst(ctx, db.UserFeedsFirstParams{
+			UserID:          userID,
+			PaginationLimit: paginationLimit,
+		})
+		if err != nil {
+			return nil, postgres.PgErrMapper(err)
+		}
+	} else {
+		rows, err = s.store.Q.UserFeedsAfter(ctx, db.UserFeedsAfterParams{
+			UserID: userID,
+			CursorCreatedAt: pgtype.Timestamptz{
+				Time:  cursor.CreatedAt,
+				Valid: true,
+			},
+			CursorID:        cursor.ID,
+			PaginationLimit: paginationLimit,
+		})
+		if err != nil {
+			return nil, postgres.PgErrMapper(err)
+		}
+	}
+
+	ps := make([]post.Post, 0, len(rows))
+	for _, p := range rows {
+		ps = append(ps, post.Post{
 			ID:        p.ID,
 			AuthorID:  p.AuthorID,
 			Title:     p.Title,
@@ -46,5 +83,28 @@ func (s *Service) GetFeeds(
 		})
 	}
 
-	return f, nil
+	hasMore := len(rows) > int(cursorQuery.Limit)
+	if hasMore {
+		ps = ps[:cursorQuery.Limit]
+	}
+
+	var nextCursor string
+	if hasMore && len(rows) > 0 {
+		lastPost := rows[len(rows)-1]
+		c := pagination.Cursor{
+			CreatedAt: lastPost.CreatedAt,
+			ID:        lastPost.ID,
+		}
+
+		nextCursor, err = pagination.EncodeCursor(c)
+		if err != nil {
+			return nil, apperr.ErrInternal
+		}
+	}
+
+	return &GetUserFeedsPayload{
+		Posts:      ps,
+		NextCursor: nextCursor,
+		HasMore:    hasMore,
+	}, nil
 }
